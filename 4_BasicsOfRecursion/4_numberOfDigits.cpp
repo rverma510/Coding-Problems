d81b9c393cfec7ff0906c4452825c302bef6c440
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-02-26 18:58:32 +0530

    Added 4_numberOfDigits.cpp

---
+++ 4_BasicsOfRecursion/4_numberOfDigits.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countNumberOfDigits(int n){
+    if(n == 0)
+        return 0;
+    return 1 + countNumberOfDigits(n/10);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int res = countNumberOfDigits(n);
+    cout<<res<<"\n";
+    return 0;
+}
