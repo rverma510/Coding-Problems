cca05edff7155d0f62717b7778e66e90ca22d56b
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-04-19 10:42:50 +0530

    Added 5_subsequences.cpp

---
+++ 8_AdvancedRecursion/5_subsequences.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int subs(string input, string output[]){
+    if(input.empty()){
+        output[0] = "";
+        return 1;
+    }
+    int smallOutput = subs(input.substr(1), output);
+    for(int i = 0; i < smallOutput; i++){
+        output[i + smallOutput] = input[0] + output[i];
+    }
+    return 2 * smallOutput;
+}
+
+int main(){
+    string input;
+    cin>>input;
+    string *output = new string[(int)pow(2, input.size())];
+    int count = subs(input, output);
+    for(int i = 0; i < count; i++){
+        cout<<output[i]<<endl;
+    }
+}
