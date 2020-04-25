b3a576e06e42693313cf3c7b6a66cd4d33b9d278
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-04-25 13:26:14 +0530

    Added 15_hasanAndTrip.cpp

---
+++ 11_DynamicProgramming/15_hasanAndTrip.cpp
@@ -0,0 +1,61 @@
+/*
+Hasan has finally finished his final exams and he decided to go in a trip among cities in Syria.
+There are N cities in Syria and they are numbered from 1 to N, each city has coordinates on plane, i-th city is in (Xi, Yi).
+Hasan is in first city and he wants to visit some cities by his car in the trip but the final destination should be N-th city and the
+sequence of cities he will visit should be increasing in index (i.e. if he is in city i he can move to city j if and only if i < j ).
+Visiting i-th city will increase Hasan's happiness by Fi units (including first and last cities), also Hasan doesn't like traveling too much,
+so his happiness will decrease by total distance traveled by him.
+Help Hasan by choosing a sequence of cities to visit which maximizes his happiness.
+
+Input format:
+First line contain integer N.
+Next N lines contains three integers each, i-th line contains coordinates of i-th city Xi, Yi and Fi.
+
+Output format:
+Output one number rounded to 6 digits after floating point, the maximum possible happiness Hasan can get.
+
+Note: If answer is 2 print 2.000000
+
+Constraints:
+1 <= N <= 3,000
+0 <= Xi, Yi, Fi <= 100,000
+
+Sample Input
+3
+0 0 1
+3 1 1
+6 0 9
+
+Sample Output
+4.675445
+*/
+#include<bits/stdc++.h>
+using namespace std;
+double solve(vector<pair<pair<double, double>, double>> cord, int n){
+    double *dp = new double[n];
+    dp[0] = cord[0].second;
+    for(int i = 1; i < n; i++){
+        dp[i] = INT_MIN;
+        for(int j = 0; j < i; j++){
+            double sx = cord[j].first.first;
+            double sy = cord[j].first.second;
+            double ex = cord[i].first.first;
+            double ey = cord[i].first.second;
+            double dis = sqrt((ex - sx) * (ex - sx) + (ey - sy) * (ey - sy));
+            dp[i] = max(dp[i], dp[j] + cord[i].second - dis);
+        }
+    }
+    return dp[n - 1];
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<pair<pair<double, double>, double>> cord;
+    for(int i = 0; i < n; i++){
+        double x, y, h;
+        cin>>x>>y>>h;
+        cord.push_back({{x, y}, h});
+    }
+    printf("%0.6f\n", solve(cord, n));
+    return 0;
+}
