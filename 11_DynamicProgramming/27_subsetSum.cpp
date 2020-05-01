817c9d178e675c74e67e0010b748a94a7434620d
Raghav Verma <42772063+WolverineXR@users.noreply.github.com>
2020-05-01 16:29:34 +0530

    Modified 27_subsetSum.cpp

--- 11_DynamicProgramming/27_subsetSum.cpp
+++ 11_DynamicProgramming/27_subsetSum.cpp
@@ -11,24 +11,22 @@ bool solve(int *input, int n, int sum){
         return solve(input, n - 1, sum) || solve(input, n - 1, sum - input[n - 1]);
 }
 bool solveDP(int *input, int n, int sum){
-    bool **dp = new bool*[n + 1];
-    for(int i = 0; i <= n; i++)
+    bool **dp = new bool*[2];
+    for(int i = 0; i <= 1; i++)
         dp[i] = new bool[sum + 1];
-    for(int i = 0; i <= n; i++){
-        for(int j = 0; j <= sum; j++){
-            if(i == 0 && j > 0)
-                dp[i][j] = false;
-        	else if(j == 0)
-                dp[i][j] = true;
-            else if(input[i] <= j){
-                dp[i][j] = dp[i - 1][j] || dp[i - 1][j - input[i]];
-            }
-            else{
-                dp[i][j] = dp[i - 1][j];
-            }
+    for(int i = 0; i <= sum; i++)
+        dp[0][i] = false;
+    dp[0][0] = true;
+    int flag = 1;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= sum; j++){
+            dp[flag][j] = dp[flag ^ 1][j];
+            if(j >= input[i])
+                dp[flag][j] = dp[flag][j] || dp[flag ^ 1][j - input[i]];
         }
+        flag = flag ^ 1;
     }
-    return dp[n][sum];
+    return dp[flag ^ 1][sum];
 }
 int main(){
     int n, sum;
