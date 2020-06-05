c278372143a4b53589d0eced55b1f9faf15c5847
rverma510 <raghavverma4091@gmail.com>
2020-06-05 16:15:37 +0530

    Added 5_NumberOfNodesGreaterThanX.cpp

---
+++ 14_Trees/5_NumberOfNodesGreaterThanX.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int val;
+    vector<Node *> child;
+};
+
+Node* newNode(int val) {
+    Node *tmp = new Node;
+    tmp -> val = val;
+    return tmp;
+}
+
+Node *takeInputLevelWise() {
+    int val, childCount;
+    cout<<"Enter node val : ";
+    cin>>val;
+    Node *root = newNode(val);
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node *p = q.front();
+        q.pop();
+        cout<<"Enter child count for "<<p->val<<" : ";
+        cin>>childCount;
+        for (int i = 0; i < childCount; i++) {
+            cout<<"Enter "<<i+1<<"th child of "<<p->val<<" : ";
+            cin>>val; 
+            Node *child = newNode(val);
+            p->child.push_back(child);
+            q.push(child);
+        }
+    }
+    return root;
+}
+
+int countNode(Node *root, int x) {
+    if (root == NULL)
+        return 0;
+    int count = root->val > x ? 1: 0;
+    for (int i = 0; i < root->child.size(); i++) {
+        count += countNode(root->child[i], x);
+    }
+    return count;
+}
+
+int main() {
+    int x;
+    cin>>x;
+    Node *root = takeInputLevelWise();
+    cout<<countNode(root, x)<<endl;
+    return 0;
+}
