68401a3aacefa9c69492188e725a4d8dd418fb3a
rverma510 <raghavverma4091@gmail.com>
2020-06-05 16:24:20 +0530

    Added 6_findHeight.cpp

---
+++ 14_Trees/6_findHeight.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int val;
+    vector<Node *> child;
+};
+
+Node* newNode(int val) {
+    Node *tmp = new Node;
+    tmp -> val = val;
+    return tmp;
+}
+
+Node *takeInputLevelWise() {
+    int val, childCount;
+    cout<<"Enter node val : ";
+    cin>>val;
+    Node *root = newNode(val);
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node *p = q.front();
+        q.pop();
+        cout<<"Enter child count for "<<p->val<<" : ";
+        cin>>childCount;
+        for (int i = 0; i < childCount; i++) {
+            cout<<"Enter "<<i+1<<"th child of "<<p->val<<" : ";
+            cin>>val; 
+            Node *child = newNode(val);
+            p->child.push_back(child);
+            q.push(child);
+        }
+    }
+    return root;
+}
+
+int height(Node *root) {
+    if (root == NULL)
+        return 0;
+    int h = 1;
+    for (int i = 0; i < root->child.size(); i++) {
+        h = max(h, height(root->child[i]) + 1);
+    }
+    return h;
+}
+
+int main() {
+    Node *root = takeInputLevelWise();
+    cout<<height(root)<<endl;
+    return 0;
+}
