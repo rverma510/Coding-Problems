71365772f46fc88c6708f13161d77f609a8a68e9
rverma510 <raghavverma4091@gmail.com>
2020-07-23 20:47:31 +0530

    Added OverloadingNewAndDelete.cpp

---
+++ Memory Management/3 Dynamic Memory Allocation/OverloadingNewAndDelete.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+class MyClass {
+    int num;
+  public:
+    MyClass() {
+        cout << "Constructor called\n";
+    }
+    ~MyClass() {
+        cout << "Destructor called\n";
+    }
+    void* operator new(size_t size) {
+        cout << "Allocating " << size << " bytes of memory\n";
+        void *p = malloc(size);
+        return p;
+    }
+    void operator delete(void *p) {
+        cout << "Deleting memory\n";
+        free(p);
+    }
+};
+
+int main() {
+    MyClass *obj = new MyClass();
+    cout << "Obj initialized\n";
+    delete obj;
+    return 0;
+}
