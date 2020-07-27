fd742a7b6c4bbc0cf993b0fe7cd03147920698ac
rverma510 <raghavverma4091@gmail.com>
2020-07-27 15:10:48 +0530

    Added shared_pointer.cpp

---
+++ Memory Management/5 Smart Pointers/shared_pointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <memory>
+
+int main() {
+    std::shared_ptr<int> shared1(new int);
+    std::cout << "Shared pointer count = " << shared1.use_count() << std::endl;
+    {
+        std::shared_ptr<int> shared2 = shared1;
+        std::cout<< "Shared pointer count = " << shared2.use_count() << std::endl;
+    }
+    std::cout << "Shared pointer count = " << shared1.use_count() << std::endl;
+    return 0;
+}
