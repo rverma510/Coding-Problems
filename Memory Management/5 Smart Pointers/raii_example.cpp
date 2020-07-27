0d7f49ec6ce4af461dbad746943f63706b8f65d8
rverma510 <raghavverma4091@gmail.com>
2020-07-27 12:58:34 +0530

    Added raii_example.cpp

---
+++ Memory Management/5 Smart Pointers/raii_example.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class MyInt {
+  private:
+    int *_p;
+  public:
+    MyInt(int *p = NULL) {_p = p;}
+    ~MyInt() {
+        std::cout << "resource " << *_p << " deallocated" << std::endl;
+        delete _p;
+    }
+    int &operator *() {
+        return *_p;
+    }
+};
+
+int main()
+{
+    double den[] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    for (size_t i = 0; i < 5; ++i)
+    {
+        // allocate the resource on the heap
+        MyInt en(new int(i));
+
+        // use the resource
+        std::cout << *en << "/" << den[i] << " = " << *en / den[i] << std::endl;
+    }
+
+    return 0;
+}
