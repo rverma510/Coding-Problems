b3aafb7a3c429a4c82d1551bc013af8eec08db0c
rverma510 <raghavverma4091@gmail.com>
2020-08-05 13:09:31 +0530

    Added 4_creatingDeadlock_1.cpp

---
+++ Concurrency/3 mutexes and locks/4_creatingDeadlock_1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <future>
+#include <mutex>
+#include <vector>
+#include <algorithm>
+
+double result;
+std::mutex _mutex;
+
+void printResult(int den) {
+    std::cout << "for denom " << den << ", the result is " << result << std::endl;
+}
+
+void divideByNumber(double num, double den) {
+    _mutex.lock();
+    try {
+        if (den != 0) {
+            result = num / den;
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            printResult(den);
+        }
+        else {
+            throw std::invalid_argument("Error! divide by zero error");
+        }
+    }
+    catch(std::invalid_argument e) {
+        std::cout << e.what() << std::endl;
+        return;
+    }
+    _mutex.unlock();
+}
+
+int main() {
+    std::vector<std::future<void>> futures;
+    for (int i = -5; i <= 5; i++) {
+        futures.emplace_back(std::async(std::launch::async, divideByNumber, 50.0, i));
+    }
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr) {
+        ftr.wait();
+    });
+    return 0;
+}
