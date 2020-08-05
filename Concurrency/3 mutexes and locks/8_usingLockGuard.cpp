d85d3bedc72acd1acc2d47a0cc9aab2b1bac266e
rverma510 <raghavverma4091@gmail.com>
2020-08-05 14:49:51 +0530

    Added 8_usingLockGuard.cpp

---
+++ Concurrency/3 mutexes and locks/8_usingLockGuard.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+#include <future>
+#include <algorithm>
+
+double result;
+std::mutex mtx;
+
+void printResult(double den) {
+    std::cout << "for denom " << den <<", the result is " << result << std::endl;
+}
+
+void divideByNumber(double num, double den) {
+    std::lock_guard<std::mutex> lck(mtx);
+    try {
+        if (den != 0) {
+            result = num / den;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            printResult(den);
+        }
+        else {
+            throw std::runtime_error("Error: Divide By Zero");
+        }
+    }
+    catch(std::runtime_error e) {
+        std::cout << e.what() << std::endl;
+        return;
+    }
+}
+
+int main() {
+    std::vector<std::future<void>> futures;
+    for (int i = -5; i <= 5; i++) {
+        futures.emplace_back(std::async(std::launch::async, divideByNumber, 50.0, i));
+    }
+    std::for_each(futures.begin(), futures.end(), [](std::future<void> &ftr){
+        ftr.wait();
+    });
+    return 0;
+}
