ac52b5d4ecfa7c5fff7b008476be8647a70de5a4
rverma510 <raghavverma4091@gmail.com>
2020-08-15 19:40:59 +0530

    Modified 54_unboundedKnapsack.cpp

--- 11_DynamicProgramming/54_unboundedKnapsack.cpp
+++ 11_DynamicProgramming/54_unboundedKnapsack.cpp
@@ -10,6 +10,21 @@ int recursiveKnapsack(vector<int> &w, vector<int> &val, int n, int W) {
         return max(val[n - 1] + recursiveKnapsack(w, val, n, W - w[n - 1]), recursiveKnapsack(w, val, n - 1, W));
 }
 
+int dynamicKnapsack(vector<int> &w, vector<int> &val, int n, int W) {
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1));
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else if (w[i - 1] <= j) 
+                dp[i][j] = max(val[i - 1] + dp[i][j - w[i - 1]], dp[i - 1][j]);
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    return dp[n][W];
+}
+
 int main() {
     int n, W;
     cin >> n;
@@ -21,6 +36,7 @@ int main() {
         cin >> val[i];
     }
     cin >> W;
-    cout << recursiveKnapsack(w, val, n, W) << endl;
+    // cout << recursiveKnapsack(w, val, n, W) << endl;
+    cout << dynamicKnapsack(w, val, n, W) << endl;
     return 0;
 }
