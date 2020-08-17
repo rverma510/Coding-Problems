3804517117a35224a858a894d204a53f54173319
rverma510 <raghavverma4091@gmail.com>
2020-08-17 13:15:34 +0530

    Modified 58_longestCommomSubsequence.cpp

--- 11_DynamicProgramming/58_LongestCommomSubsequence.cpp
+++ 11_DynamicProgramming/58_LongestCommomSubsequence.cpp
@@ -30,10 +30,28 @@ int memoizedLCS(string x, string y) {
     return ans;
 }
 
+int dynamicLCS(string x, string y) {
+    int m = x.size();
+    int n = y.size();
+    if (m == 0 || n == 0)
+        return 0;
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (x[i - 1] == y[j - 1])
+                dp[i][j] = 1 +  dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[m][n];
+}
+
 int main() {
     string x, y;
     cin >> x >> y;
     // cout << recursiveLCS(x, x.size(), y, y.size()) << endl;
-    cout << memoizedLCS(x, y) << endl;
+    // cout << memoizedLCS(x, y) << endl;
+    cout << dynamicLCS(x, y) << endl;
     return 0;
 }
