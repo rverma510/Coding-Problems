6adfa268c4d05787ff761b06da7cda59817eda6d
rverma510 <raghavverma4091@gmail.com>
2020-11-11 02:32:49 +0530

    Modified 1_RGAME.cpp

--- 21_Adhoc/1_RGAME.cpp
+++ 21_Adhoc/1_RGAME.cpp
@@ -1,25 +1,49 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <queue>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define rf freopen("in.txt", "r", stdin);
+#define wf freopen("out.txt", "w", stdout);
 using namespace std;
-typedef long long ll;
-#define mod 1000000007
-ll func(vector<ll> &arr, ll i, ll left, ll right) {
-    if (i >= arr.size())
-        return 0;
-    ll ans1 = ((ll)pow(2, arr.size() - i - 1) * right * arr[i]) + func(arr, i + 1, left, arr[i]);
-    ll ans2 = ((ll)pow(2, arr.size() - i - 1) * left * arr[i]) + func(arr, i + 1, arr[i], right);
-    return (ans1 + ans2);
-}
+const int mx = 1e5 + 10, mod = 1e9 + 7;
+int n, t;
+int a[mx];
+int suffix[mx];
+
 int main() {
-    ll t;
+    speed;
     cin >> t;
     while (t--) {
-        ll n;
         cin >> n;
-        vector<ll> arr(n + 1);
-        for (int i = 0; i <= n; i++) {
-            cin >> arr[i];
+        n++;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
         }
-        cout << func(arr, 1, arr[0], arr[0]) << "\n";
+        int power_of_2 = 1;
+        for (int i = n; i >= 1; i--) {
+            suffix[i] = (1ll * power_of_2 * a[i]) % mod;
+            if (i < n)
+                suffix[i] = (suffix[i + 1] + suffix[i]) % mod;
+            power_of_2 = (power_of_2 << 1) % mod;
+        }
+        int ans = 0;
+        power_of_2 = 1;
+        for (int i = 1; i < n; i++) {
+            int count_ways = (1ll * power_of_2 * a[i]) % mod;
+            count_ways = (1ll * count_ways * suffix[i + 1]) % mod;
+            ans = (ans + count_ways) % mod;
+            if (i >= 2)
+                power_of_2 = (power_of_2 << 1) % mod;
+        }
+        cout << (2 * ans) % mod << "\n";
     }
     return 0;
 }
