4a3ee833b118754de890c250ba540180d1ce541a
rverma510 <raghavverma4091@gmail.com>
2020-11-12 00:44:44 +0530

    Added template.cpp

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <map>
+#include <set>
+#include <string>
+#include <cstring>
+#include <queue>
+#define speed ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define rf freopen("in.txt", "r", stdin);
+#define wf freopen("out.txt", "w", stdout);
+using namespace std;
+
+typedef long long ll;
+const int mx = 1e5 + 10, mod = 1e9 + 7;
+int n, t;
+int a[mx];
+
+int main() {
+    speed;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        ll ans = 0;
+        /*
+            Logic
+                 Goes
+                     Here
+        */
+        cout << ans << "\n";
+    }
+    return 0;
+}
