b1c53235094c703324eb39b02744d3eacc2b9685
rverma510 <raghavverma4091@gmail.com>
2020-12-01 17:31:02 +0530

    Added 5_TanyaAndToys.cpp

---
+++ 23_Greedy/5_TanyaAndToys.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speed ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define rf freopen("in.txt", "r", stdin);
+#define wf freopen("out.txt", "w", stdout);
+
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define mem(a, b) memset(a, b, sizeof(a))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7;
+
+const double pi = acos(-1.0);
+const int maxn = 2e5 + 10;
+const int dir[][2]={{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+ll n, m;
+set<int> s;
+void solve() {
+    cin >> n >> m;
+    for (int tmp, i = 0; i < n; i++) {
+        cin >> tmp;
+        s.insert(tmp);
+    }
+    vector<int> ans;
+    for (int i = 1; i <= m; i++) {
+        if (s.find(i) == s.end()) {
+            ans.pb(i);
+            m -= i;
+        }
+    }
+    cout << ans.size() << endl;
+    for (auto i: ans) cout << i << ' ';
+    cout << endl;
+}
+
+int main() {
+    speed;
+    solve();
+    return 0;
+}
