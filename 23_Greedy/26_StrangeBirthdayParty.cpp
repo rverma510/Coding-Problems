36eb9813a6ae7b98eebb56efd04fe598f04ef825
rverma510 <raghavverma4091@gmail.com>
2021-01-06 11:40:05 +0530

    Added 26_StrangeBirthdayParty.cpp

---
+++ 23_Greedy/26_StrangeBirthdayParty.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speed ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define rf freopen("in.txt", "r", stdin);
+#define wf freopen("out.txt", "w", stdout);
+
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+#define mem(a, b) memset(a, b, sizeof(a))
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int mod = 1e9 + 7;
+
+const double pi = acos(-1.0);
+const int maxn = 3e5 + 10;
+const int dir[][2]={{0, 1}, {1, 0}, {0, -1}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+
+int t, n, m;
+int k[maxn];
+int c[maxn];
+
+void solve() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> k[i];
+        k[i]--;
+    }
+    for (int i = 0; i < m; i++) cin >> c[i];
+    sort(k, k + n);
+    int idx = 0;
+    ll ans = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (idx <= k[i]) {
+            ans += c[idx];
+            idx++;
+        }
+        else ans += c[k[i]];
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    speed;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
