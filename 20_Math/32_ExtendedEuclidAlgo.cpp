7b31dff9eb89d32d013d71be360ef3b5363eda9d
rverma510 <raghavverma4091@gmail.com>
2021-06-21 12:47:56 +0530

    Added 32_ExtendedEuclidAlgo.cpp

---
+++ 20_Math/32_ExtendedEuclidAlgo.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Triplet {
+    int x, y, gcd;
+};
+
+Triplet extendedEuclid(int a, int b) {
+    if (a < b) return extendedEuclid(b, a);
+
+    if (b == 0) {
+        Triplet res;
+        res.gcd = a;
+        res.x = 1;
+        res.y = 0;
+        return res;
+    } 
+
+    Triplet ans = extendedEuclid(a, a % b);
+    Triplet res;
+    res.gcd = ans.gcd;
+    res.x = ans.y;
+    res.y = (ans.x - (a / b) * ans.y);
+    return res;
+}
+
+int main() {
+    int a = 42;
+    int b = 18;
+    Triplet res =  extendedEuclid(a, b);
+    cout << "X and y of ax + by = gcd(a, b) : " << res.x << ' ' << res.y << '\n';
+    return 0;
+}
