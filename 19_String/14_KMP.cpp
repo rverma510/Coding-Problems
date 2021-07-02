da2680ca2441c34872aab91cbe6b81be79d5012a
rverma510 <raghavverma4091@gmail.com>
2021-07-02 11:48:51 +0530

    Added 14_KMP.cpp

---
+++ 19_String/14_KMP.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int maxn = 1e6 + 10;
+int lps[maxn];
+void lpsHelper(string pat) {
+    lps[0] = 0;
+    int i = 1, j = 0;
+    while (i < pat.length()) {
+        if (pat[i] == pat[j]) {
+            lps[i] = j + 1;
+            i++;
+            j++;
+        }
+        else {
+            if (j == 0) {
+                lps[i] = 0;
+                i++;
+            }
+            else {
+                j = lps[j - 1];
+            }
+        }
+    }
+} 
+
+int KMP(string text, string pat) {
+    lpsHelper(pat);
+    int n = text.length();
+    int m = pat.length();
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (pat[j] == text[i]) {
+            i++;
+            j++;
+        }
+        else {
+            if (j == 0) {
+                i++;
+            }
+            else {
+                j = lps[j - 1];
+            }
+        }
+    }
+    if (j == m) {
+        return i - j; 
+    }
+    return -1;
+}
+
+int main() {
+    string text, pat;
+    cin >> text >> pat;
+    cout << KMP(text, pat) << '\n';
+    return 0;
+}
